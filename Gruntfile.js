fbecbb73c99d22186b0095a43a35d0cc7ada8f77
STEPANYANA <a.stepanyan@elsevier.com>
2016-05-03 15:01:27 +0200

    Adding main processing files

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+
+'use strict';
+/**
+ * @file Gruntfiles.js
+ * main file for managing grunt configuration
+ * @author Albert Cyberhulk
+ * @date 03.05.2016
+ */
+
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
+
+module.exports = function (grunt) {
+
+};
